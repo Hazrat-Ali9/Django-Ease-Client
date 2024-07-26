79796762f7d579440aa9d150f9f152602c3a8586
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-07-26 20:31:16 +0600

    main js :truck:

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import AuthProvider from "./providers/AuthProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
-// import 'flowbite';
+// import 'Flowbite';
 
 const queryClient = new QueryClient();
 
