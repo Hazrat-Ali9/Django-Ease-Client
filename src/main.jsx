dd2724de1101525ef4112081c7b94652e5253c73
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-08-24 22:55:24 +0600

    code added :tada:

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import AuthProvider from "./providers/AuthProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
-// import 'Flowbite';
+// Import 'Flow Bite';
 
 const queryClient = new QueryClient();
 // React Strict Mode 
--- src/pages/Register/DistrictAndUpazila.jsx
+++ src/pages/Register/DistrictAndUpazila.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-
+// District
 const DistrictAndUpazila = () => {
 	const { data = {}, isLoading } = useQuery({
 		queryKey: ["district-upzila"],
