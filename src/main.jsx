e6e560781ca2debb2208ccfcb1f717362606e820
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-08-26 10:55:21 +0600

    Code add :fire:

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
 // Import 'Flow Bite';
-
+// </React.StrictMode>
 const queryClient = new QueryClient();
 // React Strict Mode 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -20,5 +20,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				</AuthProvider>
 			</QueryClientProvider>
 		</HelmetProvider>
-	// </React.StrictMode>
+	
 );
